e5eda0039c1592128c5a8da6c9d47bc7dce3be9e
chenbishop <chen.bishop@gmail.com>
2023-07-27 12:47:23 +0100

    fixing the issue where the pod restart whenever kubernetes watch api closes the connection

--- app.mjs
+++ app.mjs
@@ -17,57 +17,62 @@ dotenvPkg.config();
 let [remoteNetwork, domainList, group] = [process.env.TG_REMOTE_NETWORK, process.env.DOMAIN_LIST.split(","), process.env.TG_GROUP_NAME];
 
 const main = async () => {
-    try {
-        const utilManager = new TwingateUtilManager();
+    // while (true) {
+        try {
+            const utilManager = new TwingateUtilManager();
 
-        const remoteNetworkId = await utilManager.lookupRemoteNetworkByName(remoteNetwork);
+            const remoteNetworkId = await utilManager.lookupRemoteNetworkByName(remoteNetwork);
 
-        const groupId = await utilManager.lookupGroupByName(group);
+            const groupId = await utilManager.lookupGroupByName(group);
 
-        // Get all resources in the remote network
-        const resources = await utilManager.fetchAllResourcesInRemoteNetwork(remoteNetworkId);
+            // Get all resources in the remote network
+            let resources = await utilManager.fetchAllResourcesInRemoteNetwork(remoteNetworkId);
+            // Start watch for K8S ingress changes
+            const req = await watch.watch(
+                '/apis/networking.k8s.io/v1/ingresses',
+                {allowWatchBookmarks: true},
+                async (type, apiObj) => {
+                    if (type === 'ADDED') {
+                        const host = apiObj.spec.rules[0].host;
 
-        // Start watch for K8S ingress changes
-        const req = await watch.watch(
-            '/apis/networking.k8s.io/v1/ingresses',
-            {},
-            async (type, apiObj) => {
-                if (type === 'ADDED') {
-                    const host = apiObj.spec.rules[0].host;
+                        // Check if the ingress host is part of the domain list
+                        if (domainList.filter(domainList => host.endsWith(domainList)).length !== 0) {
 
-                    // Check if the ingress host is part of the domain list
-                    if (domainList.filter(domainList => host.endsWith(domainList)).length !== 0) {
+                            // Check if resource already exists in the remote network
+                            if (!resources.map(resource => resource.address.value).includes(host)) {
+                                // Create resource in Twingate with
+                                // resource name: ingress name
+                                // resource address: ingress first rule's host
+                                // resource group: predefined group name
+                                await utilManager.createResource(apiObj.metadata.name, host, remoteNetworkId, undefined, groupId);
+                                console.log(`New Ingress Found: creating resource '${host}' with name '${apiObj.metadata.name}' in remote network ${remoteNetwork}`);
 
-                        // Check if resource already exists in the remote network
-                        if (!resources.map(resource => resource.address.value).includes(host)) {
-                            // Create resource in Twingate with
-                            // resource name: ingress name
-                            // resource address: ingress first rule's host
-                            // resource group: predefined group name
-                            await utilManager.createResource(apiObj.metadata.name, host, remoteNetworkId, undefined, groupId);
-                            console.log(`New Ingress Found: creating resource '${host}' with name '${apiObj.metadata.name}' in remote network ${remoteNetwork}`);
+                                // refresh the remote network resources
+                                resources = await utilManager.fetchAllResourcesInRemoteNetwork(remoteNetworkId);
+                            }
+                            else {
+                                console.log(`Skipping: resource '${host}' with name '${apiObj.metadata.name}' has already been created in remote network ${remoteNetwork} previously.`);
+                            }
                         }
                         else {
-                            console.log(`Skipping: resource '${host}' with name '${apiObj.metadata.name}' has already been created in remote network ${remoteNetwork} previously.`);
+                            console.log(`Skipping: ingress ${apiObj.metadata.name} is not part of domain list.`);
                         }
                     }
                     else {
-                        console.log(`Skipping: ingress ${apiObj.metadata.name} is not part of domain list.`);
+                        console.log('unknown type: ' + type);
                     }
-                }
-                else {
-                    console.log('unknown type: ' + type);
-                }
-            },
-            // done callback is called if the watch terminates normally
-            (err) => console.error(err),
-        );
-        setTimeout(() => {
-        }, 5000);
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+                },
+                // done callback is called if the watch terminates normally
+                (err) => main(),
+            );
+
+            setTimeout(() => { }, 5000);
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    // }
+
 };
 
 main();
